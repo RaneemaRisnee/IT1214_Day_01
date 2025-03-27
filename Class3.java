bc31cbc578a19e35492bb92ae697c6d3f6d5c989
Raneema Risnee <144576507+RaneemaRisnee@users.noreply.github.com>
2025-03-27 05:59:57 +0530

    Add files via upload

---
+++ Class3.java
@@ -0,0 +1,10 @@
+package p2;
+//same as directory (folder)name 
+
+public class Class3{
+	// here class MyClass is same as file name
+	public void display(){
+		System.out.println("Hi from Class3 in package p2");
+	}
+	
+}
